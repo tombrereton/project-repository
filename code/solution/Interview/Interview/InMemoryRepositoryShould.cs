cfb19d5d02c69623c128d14d56901c3faafe3c24
Tom Brereton <Tom.Brereton@asos.com>
2019-10-05 18:18:35 +0100

    Add test for providing all entities

--- code/solution/Interview/Interview/InMemoryRepositoryShould.cs
+++ code/solution/Interview/Interview/InMemoryRepositoryShould.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Interview
@@ -9,8 +11,44 @@ namespace Interview
     internal class InMemoryRepositoryShould
     {
         [Test]
-        public void ProvideAllEntities()
+        public void ProvideAllEntitiesAsCorrectType()
         {
+            var repository =  new InMemoryRepository<Storeable>();
+
+            var actual = repository.All();
+
+            actual.Should().BeOfType<List<Storeable>>();
         }
+
+    }
+
+    internal class InMemoryRepository<T> : IRepository<T>, IDataContext<T> where T : IStoreable
+    {
+        public InMemoryRepository()
+        {
+            Data = new List<T>();
+        }
+
+        public IEnumerable<T> All()
+        {
+            return Data;
+        }
+
+        public void Delete(IComparable id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(IComparable id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Data { get; set; }
     }
 }
