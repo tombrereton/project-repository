f107d246e4cfe13ae85e12091b47d3e408ec3cd6
Tom Brereton <Tom.Brereton@asos.com>
2019-10-05 17:56:40 +0100

    set up nunit

---
+++ code/solution/Interview/Interview/InMemoryRepositoryShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Interview
+{
+    [TestFixture]
+    internal class InMemoryRepositoryShould
+    {
+        [Test]
+        public void ProvideAllEntities()
+        {
+        }
+    }
+}
